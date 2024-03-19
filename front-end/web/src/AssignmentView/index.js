d75bac08b6165647b1b2084cb0c6382ce8a1bda0
IlyaneDelor <ilyanedelor7@gmail.com>
2024-03-20 00:53:00 +0100

    Update Enums

--- front-end/web/src/AssignmentView/index.js
+++ front-end/web/src/AssignmentView/index.js
@@ -19,9 +19,11 @@ const AssignmentView = () => {
   const [assignment, setAssignment] = useState({
     githubUrl: "",
     branch: "",
+    number: null,
   });
 
-  const [assignmentEnums, setAssignmentEnums] = useState();
+  const [assignmentEnums, setAssignmentEnums] = useState([]);
+  const [selectedAssignment, setSelectedAssignment] = useState(null);
 
   const [jwt, setJwt] = useLocalState("", "jwt");
 
@@ -60,7 +62,11 @@ const AssignmentView = () => {
     <Container className="mt-5">
       <Row className="d-flex align-items-center">
         <Col>
-          <h1> Assignment {assignmentId} </h1>
+          {assignment.number ? (
+            <h1>Assignment {assignment.number} </h1>
+          ) : (
+            <h1>No assignment selected</h1>
+          )}
         </Col>
         <Col>
           <Badge pill bg="info" style={{ fontSize: "1em" }}>
@@ -71,16 +77,23 @@ const AssignmentView = () => {
 
       {assignment ? (
         <>
-          <Form.Group as={Row} className="my-3" controlId="formPlaintextEmail">
+          <Form.Group as={Row} className="my-3" controlId="assignmentName">
             <Form.Label column sm="3" md="2">
               Assignment Number :
             </Form.Label>
             <Col sm="9" md="8" lg="6">
               <DropdownButton
                 as={ButtonGroup}
-                id="assignmentName"
                 variant={"info"}
-                title="Assignment 1"
+                title={
+                  assignment.number
+                    ? `Assignment ${assignment.number}`
+                    : "Select an assignment"
+                }
+                onSelect={(selectedElement) => {
+                  setSelectedAssignment(selectedElement);
+                  updateAssignment("number", selectedElement);
+                }}
               >
                 {assignmentEnums &&
                   assignmentEnums.map((assignmentEnum) => (
@@ -91,16 +104,30 @@ const AssignmentView = () => {
               </DropdownButton>
             </Col>
           </Form.Group>
-          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+
+          <Form.Group as={Row} className="mb-3" controlId="githubUrl">
             <Form.Label column sm="3" md="2">
               Github URL:
             </Form.Label>
             <Col sm="9" md="8" lg="6">
               <Form.Control
-                id="branch"
-                onChange={(e) => updateAssignment("branch", e.target.value)}
-                type={assignment.branch}
+                onChange={(e) => updateAssignment("githubUrl", e.target.value)}
+                value={assignment.githubUrl}
+                type="url"
                 placeholder="http://github.com/username/repo-name"
+              />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="branch">
+            <Form.Label column sm="3" md="2">
+              Github URL:
+            </Form.Label>
+            <Col sm="9" md="8" lg="6">
+              <Form.Control
+                onChange={(e) => updateAssignment("branch", e.target.value)}
+                value={assignment.branch}
+                type="text"
+                placeholder="example_branch_name"
               />
             </Col>
           </Form.Group>
